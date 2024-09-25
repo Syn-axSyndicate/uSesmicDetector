18a9cf4bd36ffad0c21c673b4bf512c0a4ba2edc
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-09-26 01:38:53 +0530

    first commit

---
+++ build/CMakeFiles/uSesmicDetector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uSesmicDetector.dir/src/main.cpp.o"
+  "CMakeFiles/uSesmicDetector.dir/src/main.cpp.o.d"
+  "bin/uSesmicDetector"
+  "bin/uSesmicDetector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uSesmicDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+Author Rachit Pandey 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    printf("hello there!");
+}
